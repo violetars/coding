29ba4afe750faaa006488ecb9042825e85dac1ab
Violeta Recio <violeta.recio@kairosds.com>
2021-07-25 17:57:01 +0200

    first commit

---
+++ 03-coding/Controller/ThridViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThridViewController.swift
+//  03-coding
+//
+//  Created by Violeta Recio Sansón on 18/09/2020.
+//  Copyright © 2020 Violeta Recio Sansón. All rights reserved.
+//
+
+import UIKit
+
+class ThridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
